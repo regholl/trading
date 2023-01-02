c072b58e00bf5fc32c15d20ec6fe138d21412ea9
Prayuj Tuli <prayujtuli@hotmail.com>
2023-01-02 14:22:52 -0500

    Added trading support and made rsi and macd calculations more accurate

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import psycopg2 as pg
 from questdb.ingress import Sender, IngressError
 
 import pandas as pd
+import pandas_ta as ta
 import numpy as np
 
 import warnings
@@ -26,33 +27,60 @@ class Database:
         warnings.filterwarnings('ignore')
 
         self.NUM_BARS = num_bars
-        self.DB_HOST = config["DB_HOST"]
-        self.DB_PORT = config["DB_INSERT_PORT"]
+        self.DB_HOST = config['DB_HOST']
+        self.DB_INSERT_PORT = config['DB_INSERT_PORT']
+        self.DB_USER = config['DB_USER']
+        self.DB_PASSWORD = config['DB_PASSWORD']
+        self.DB_PORT = config['DB_PORT']
 
         self.connection = pg.connect(
             "user='{0}' password='{1}' host='{2}' port='{3}'".format(
-                config['DB_USER'],
-                config['DB_PASSWORD'],
-                config['DB_HOST'],
-                config['DB_PORT']
+                self.DB_USER,
+                self.DB_PASSWORD,
+                self.DB_HOST,
+                self.DB_PORT
             )
         )
         for ticker in tickers:
             self.bars[ticker] = pd.DataFrame(columns=['open', 'close', 'low', 'high'])
 
     def get_current_price(self, ticker):
-        query = 'SELECT LAST_PRICE FROM {0} ORDER BY timestamp DESC LIMIT 1;'.format(ticker)
-        data = pd.read_sql(query, con=self.connection)
+        try:
+            query = 'SELECT LAST_PRICE FROM {0} ORDER BY timestamp DESC LIMIT 1;'.format(ticker)
+            data = pd.read_sql(query, con=self.connection)
+        except Exception:
+            print('Connection lost. Reconnecting...')
+            self.connection = pg.connect(
+                "user='{0}' password='{1}' host='{2}' port='{3}'".format(
+                    self.DB_USER,
+                    self.DB_PASSWORD,
+                    self.DB_HOST,
+                    self.DB_PORT
+                )
+            )
+            return self.get_current_price(ticker)
 
         price = data['LAST_PRICE']
         return price.iloc[0]
 
     def get_current_bar(self, ticker):
-        query = 'SELECT timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')) floor, to_timezone(timestamp, \'-05:00\') timestamp_est,LAST_PRICE from {0} \
-                WHERE to_timezone(timestamp, \'-05:00\') \
-                BETWEEN timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')) \
-                AND dateadd(\'m\', 1, timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')))'.format(ticker)
-        data = pd.read_sql(query, con=self.connection)
+        try:
+            query = 'SELECT timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')) floor, to_timezone(timestamp, \'-05:00\') timestamp_est,LAST_PRICE from {0} \
+                    WHERE to_timezone(timestamp, \'-05:00\') \
+                    BETWEEN timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')) \
+                    AND dateadd(\'m\', 1, timestamp_floor(\'m\', to_timezone(now(), \'-05:00\')))'.format(ticker)
+            data = pd.read_sql(query, con=self.connection)
+        except Exception:
+            print('Connection lost. Reconnecting...')
+            self.connection = pg.connect(
+                "user='{0}' password='{1}' host='{2}' port='{3}'".format(
+                    self.DB_USER,
+                    self.DB_PASSWORD,
+                    self.DB_HOST,
+                    self.DB_PORT
+                )
+            )
+            return self.get_current_bar(ticker)
 
         prices = data['LAST_PRICE']
         if len(prices) == 0:
@@ -62,18 +90,28 @@ class Database:
         close = prices.tail(1).iloc[0]
         low = prices.min()
         high = prices.max()
-
-        # start_time = data['floor'].iloc[0]
 
         return [open_, close, low, high]
 
     def get_bar(self, ticker, timestamp):
-        query = 'SELECT timestamp_floor(\'m\', \'{0}\') floor, to_timezone(timestamp, \'-05:00\') timestamp_est,LAST_PRICE \
-                FROM {1} \
-                WHERE to_timezone(timestamp, \'-05:00\') \
-                BETWEEN timestamp_floor(\'m\', \'{0}\') \
-                AND dateadd(\'m\', 1, timestamp_floor(\'m\', \'{0}\'))'.format(timestamp, ticker)
-        data = pd.read_sql(query, con=self.connection)
+        try:
+            query = 'SELECT timestamp_floor(\'m\', \'{0}\') floor, to_timezone(timestamp, \'-05:00\') timestamp_est,LAST_PRICE \
+                    FROM {1} \
+                    WHERE to_timezone(timestamp, \'-05:00\') \
+                    BETWEEN timestamp_floor(\'m\', \'{0}\') \
+                    AND dateadd(\'m\', 1, timestamp_floor(\'m\', \'{0}\'))'.format(timestamp, ticker)
+            data = pd.read_sql(query, con=self.connection)
+        except Exception:
+            print('Connection lost. Reconnecting...')
+            self.connection = pg.connect(
+                "user='{0}' password='{1}' host='{2}' port='{3}'".format(
+                    self.DB_USER,
+                    self.DB_PASSWORD,
+                    self.DB_HOST,
+                    self.DB_PORT
+                )
+            )
+            return self.get_bar(ticker, timestamp)
 
         prices = data['LAST_PRICE']
         if len(prices) == 0:
@@ -83,8 +121,6 @@ class Database:
         close = prices.tail(1).iloc[0]
         high = prices.max()
         low = prices.min()
-
-        # start_time = data['floor'].iloc[0]
 
         return [open_, close, low, high]
 
@@ -96,7 +132,7 @@ class Database:
 
         if len(self.bars[ticker]) > self.NUM_BARS:
             self.bars[ticker] = self.bars[ticker].iloc[1:, :]
-        while last_time < pd.Timestamp.now().floor('T'):
+        while last_time < pd.Timestamp.now().floor('T')
             bar = self.get_bar(ticker, last_time)
 
             # if the queue is full, drop the first row through FIFO principal
@@ -131,9 +167,12 @@ class Database:
         return (current_price * k) + \
             ((1 - k) * self.calculate_ema_recursive(ticker, count - 1, timestamp - pd.Timedelta(1, 'm')))
 
+    # def calculate_macd(self, ticker):
+    #     current_time = self.bars[ticker].iloc[-1:].index
+    #     return self.calculate_macd_at_timestamp(ticker, current_time)
+
     def calculate_macd(self, ticker):
-        current_time = self.bars[ticker].iloc[-1:].index
-        return self.calculate_macd_at_timestamp(ticker, current_time)
+        return ta.momentum.macd(self.bars[ticker]['close'])['MACDh_12_26_9'].iloc[-1]
 
     def calculate_macd_signal_line(self, ticker):
         current_time = self.bars[ticker].iloc[-1:].index
@@ -150,47 +189,50 @@ class Database:
     def calculate_macd_at_timestamp(self, ticker, timestamp):
         return self.calculate_ema(ticker, 12, timestamp) - self.calculate_ema(ticker, 26, timestamp)
 
+    # def calculate_rsi(self, ticker, periods=14):
+    #     diffs = self.bars[ticker].diff(1)['close'].iloc[self.NUM_BARS - periods:]
+    #     gains = diffs.clip(lower=0)
+    #     losses = diffs.clip(upper=0).abs()
+    #     avg_gain = gains.rolling(window=periods, min_periods=periods).mean()[:periods+1]
+    #     avg_loss = losses.rolling(window=periods, min_periods=periods).mean()[:periods+1]
+
+    #     for i, row in enumerate(avg_gain.iloc[periods+1:]):
+    #         avg_gain.iloc[i + periods + 1] =\
+    #             (avg_gain.iloc[i + periods] *
+    #             (periods - 1) +
+    #             gains.iloc[i + periods + 1])\
+    #             / periods
+
+    #     for i, row in enumerate(avg_loss.iloc[periods+1:]):
+    #         avg_loss.iloc[i + periods + 1] =\
+    #             (avg_loss.iloc[i + periods] *
+    #             (periods - 1) +
+    #             losses.iloc[i + periods + 1])\
+    #             / periods
+
+    #     rs = avg_gain / avg_loss
+    #     rsi = 100 - (100 / (1.0 + rs))
+
+    #     try:
+    #         return rsi.iloc[periods - 1]
+    #     except Exception:
+    #         return None
+
     def calculate_rsi(self, ticker, periods=14):
-        diffs = self.bars[ticker].diff(1)['close'].iloc[self.NUM_BARS - periods:]
-        gains = diffs.clip(lower=0)
-        losses = diffs.clip(upper=0).abs()
-        avg_gain = gains.rolling(window=periods, min_periods=periods).mean()[:periods+1]
-        avg_loss = losses.rolling(window=periods, min_periods=periods).mean()[:periods+1]
-
-        for i, row in enumerate(avg_gain.iloc[periods+1:]):
-            avg_gain.iloc[i + periods + 1] =\
-                (avg_gain.iloc[i + periods] *
-                (periods - 1) +
-                gains.iloc[i + periods + 1])\
-                / periods
-
-        for i, row in enumerate(avg_loss.iloc[periods+1:]):
-            avg_loss.iloc[i + periods + 1] =\
-                (avg_loss.iloc[i + periods] *
-                (periods - 1) +
-                losses.iloc[i + periods + 1])\
-                / periods
-
-        rs = avg_gain / avg_loss
-        rsi = 100 - (100 / (1.0 + rs))
-
-        try:
-            return rsi.iloc[periods - 1]
-        except Exception:
-            return None
+        return ta.momentum.rsi(close=self.bars[ticker]['close'], length=periods).iloc[-1]
 
     def reset_trades(self):
         query = 'DROP TABLE IF EXISTS Trades;'
         with self.connection.cursor() as cur:
             cur.execute(query)
 
-        query = 'CREATE TABLE Trades(ticker string, profit float, time timestamp);'
+        query = 'CREATE TABLE Trades(ticker string, profit float, time timestamp) timestamp(time);'
         with self.connection.cursor() as cur:
             cur.execute(query)
 
     def send_trade(self, ticker, profit):
         try:
-            with Sender(self.DB_HOST, self.DB_PORT) as sender:
+            with Sender(self.DB_HOST, self.DB_INSERT_PORT) as sender:
                 sender.row(
                     'Trades',
                     columns={
--- main.py
+++ main.py
@@ -12,31 +12,33 @@ if __name__ == '__main__':
     database = Database(tickers, 64)
     database.reset_trades()
 
-    # tickers = ['AAPL']
+    tickers = ['AAPL']
 
     def algo(ticker):
         while len(database.bars[ticker]) != database.NUM_BARS:
             database.update_bars(ticker)
             # print(database.bars[ticker])
-
+        print('Finished initialization for {0}'.format(ticker))
         while True:
             database.update_bars(ticker)
-            # print(database.bars[ticker])
-            # print('{0}: {1}'.format(ticker, database.calculate_rsi(ticker)))
-            # print('{0}: {1}'.format(ticker, database.calculate_macd(ticker)))
             rsi = database.calculate_rsi(ticker)
             macd = database.calculate_macd(ticker)
+
+            print('{0}: {1}'.format(ticker, rsi))
+            print('{0}: {1}'.format(ticker, macd))
+            print(rsi, macd)
 
             # check if past three bars are negative? maybe
             if not (math.isnan(rsi) or math.isnan(macd)):
                 if rsi > 50 and macd > 0.01:
                     buy_price = database.get_current_price(ticker)
-                    print('Bought {0} at {1}'.format(ticker, buy_price))
+                    num_shares = math.ceil(2000 / buy_price)
+                    print('Bought {0} shares of {1} at {2}'.format(num_shares, ticker, buy_price))
                     while True:
                         current_price = database.get_current_price(ticker)
                         if current_price > (1.001 * buy_price) or current_price < (0.999 * buy_price):
-                            print('Sold {0} at {1} for a {2} of {3}'.format(ticker, current_price, 'profit' if current_price > buy_price else 'loss', abs(current_price - buy_price)))
-                            database.send_trade(ticker, current_price - buy_price)
+                            print('Sold {0} at {1} for a {2} of {3}'.format(ticker, current_price, 'profit' if current_price > buy_price else 'loss', abs(current_price - buy_price) * num_shares))
+                            database.send_trade(ticker, (current_price - buy_price) * num_shares)
                             break
 
     algo_threads = []
